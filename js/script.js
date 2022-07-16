5d1abd02985ddc00ef2ec1fe7c302eb5b7df38c6
diver <dn123581@gmail.com>
2022-07-16 17:09:02 +0700

    update js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
 
-var form = document.getElementById("form");
+const form = document.getElementById("form");
 
 form.addEventListener("submit", checkID);
 
 function checkID() {
-    var fullname = document.getElementById("fullname").value;
-    document.getElementById("result").innerHTML = fullname;
-
+    const fullname = document.getElementById("fullname").value;
+    const birthdate = document.getElementById("birthdate").value;
+    const id = document.getElementById("id").value;
+    const url = "https://dichvucong.yenbai.gov.vn/dichvucong/cong-dan/thong_tin_cong_dan";
+    const data = "txt_CMND_CONG_DAN=" + id + "&txt_HO_TEN=" + fullname + "&txt_NGAY_SINH=" + birthdate
+    const xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+        document.getElementById("result").innerHTML = this.responseText;
+    }
+    xhr.open("POST", url);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.send(data);
 }
